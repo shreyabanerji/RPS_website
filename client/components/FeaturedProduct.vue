2ff130a1118d8d2842ca05f9d18e23910b5286c6
shreyabanerji <shreyabanerjee167@gmail.com>
2020-04-03 22:07:25 +0530

    added main page

---
+++ client/components/FeaturedProduct.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="mainContent">
+        <div class="unified_widget pageBanner">
+            <h1>Hotels</h1>
+            <p>
+                <b>
+                    Browse for more Hotels
+                    <a href="#">Hotels in Delhi</a>
+                    <a href="#"> Rated more than 4.5 stars</a>
+                </b>
+            </p>
+        </div>
+        <div class="bx-root">
+            <div class="row">
+                <div class="col-sm-5 text-center">
+                    <div class="bx-root-image">
+                        <img src="/img/hotel_1.jpg" class="img-fluid"/>
+                    </div>
+                </div>
+                <div class="col-sm-7 m-auto">
+                    <div class="bx-root-heading">The Program</div>
+                </div>
+                <div class="bx-root-subtext">
+                    Write Something here
+                </div>
+                <div class="bx-root-link">
+                    <span> Book now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,91 @@
 <template>
-<!--
-  <div class="container">
-    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
-    </div>
--->
-<div></div>
-</template>
+  <main class="listingPage">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+          <!--Sidebar-->
+        </div>
 
+        <!--Main Content-->
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <FeaturedProduct/>
+
+          <div class="mainResults">
+            <ul class="s-result-list">
+              <li class="s-result-item celwidget">
+                <div class="s-item-container">
+                  <!--Best Rated-->
+                  <div class="a-spacing-micro"> 
+                    <div class="bestSeller">
+                      <a href="#">Best Rated</a>
+                    </div>
+                  </div>
+                  <div>
+                    <div class="row">
+                      <!--Image-->
+                      <div class="col-sm-3 text-center"> 
+                          <a href="#">
+                            <img src="/img/hotel_1.jpg" class="img-fluid"/><!--product.photo-->
+                          </a>
+                      </div>
+                      <div class="col-sm-9">
+                        <div class="a-row-spacing-small">
+                          <!--Date-->
+                          <a href="#" class="a-link-normal">
+                            <h2 class="a-size-medium">
+                              RPS HOTELS
+                              <span class="a-letter-space"></span>
+                              <span class="a-letter-space"></span>
+                              <span class="a-size-small a-color-secondary">Apr 3,2020</span>
+                            </h2>
+                          </a>
+                        </div>
+                        <!--City-->
+                        <div class="a-row a-spacing-small">
+                          City
+                        </div>
+                        <!--Price-->
+                        <div class="a-row-spacing-none">
+                          <a href="#" class="a-link-normal a-text-normal">
+                          <span class="a-offscreen">$99</span>
+                            <span class="a-color-base sx-zero-spacing">
+                            <span class="sx-price sx-price-large">
+                              <sup class="sx-price-company">$</sup>
+                              <span class="sx-price-whole">99</span>
+                              <sup class="sx-price-fractional">00</sup>
+                            </span>
+                          </span>
+                          </a>
+                          <span class="a-letter-space"></span>
+                          <span class="a-size-base-plus a-color-secondary a-text-strike">$28</span>
+
+                        </div>
+                        <!--Ratings-->
+                        <div class="col-sm-5">
+                          <div class="a-row a-spacing-mini">
+                            <!--Star ratings-->
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>  
+          </div>
+        </div>
+      </div>
+    </div>
+    </main>
+
+
+  </template>
+
+  <script>
+  import FeaturedProduct from "~/components/FeaturedProduct";
+  export default {
+    componenets:{
+      FeaturedProduct
+    }
+  };
+  </script>
